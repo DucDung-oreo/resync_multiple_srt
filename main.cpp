2aecb3e993bb5ffa46525409b6f1d3ebf489a72d
DucDung-oreo <huynhducdung1399@gmail.com>
2023-02-03 00:36:19 +0700

    Add more functions to the code
    	Remove cache for bin/ and obj/

--- main.cpp
+++ main.cpp
@@ -7,29 +7,67 @@
 
 using namespace std;
 
+struct Timestamp {
+    int hour;
+    int minu;
+    int sec;
+    int mili;
+};
+
+int totalMili(int hour, int minu, int sec, int mili)
+{
+    return (mili + 1000*sec + 1000*60*minu + 1000*60*60*hour);
+}
+
+Timestamp stringToTimestamp(string strTime)
+{
+    Timestamp t_stamp;
+
+    t_stamp.hour = stoi(strTime.substr(0,2));
+    t_stamp.minu= stoi(strTime.substr(3,2));
+    t_stamp.sec = stoi(strTime.substr(6,2));
+    t_stamp.mili = stoi(strTime.substr(9,3));
+
+    return t_stamp;
+}
+
+Timestamp breakdownMili(int sum_in_mili)
+{
+    Timestamp t_stamp;
+
+    t_stamp.hour = sum_in_mili/3600000;
+    t_stamp.minu = (sum_in_mili - t_stamp.hour*3600000)/60000;
+    t_stamp.sec = (sum_in_mili - t_stamp.hour*3600000 - t_stamp.minu*60000)/1000;
+    t_stamp.mili = (sum_in_mili - t_stamp.hour*3600000 - t_stamp.minu*60000 - t_stamp.sec*1000);
+
+    return t_stamp;
+}
+
+string timestampToString(Timestamp t_stamp)
+{
+    stringstream ss;
+    ss << setw(2) << setfill('0') << to_string(t_stamp.hour)
+    << ":" << setw(2) << setfill('0') << to_string(t_stamp.minu)
+    << ":" << setw(2) << setfill('0') << to_string(t_stamp.sec)
+    << "," << setw(3) << setfill('0') << to_string(t_stamp.mili);
+    string s = ss.str();
+    return s;
+}
+
 string increaseTime(string strTime, int delayTime)
 {
-    int hour = stoi(strTime.substr(0,2));
-    int minu = stoi(strTime.substr(3,2));
-    int sec = stoi(strTime.substr(6,2));
-    int mili = stoi(strTime.substr(9,3));
+    Timestamp t_stamp;
+    Timestamp new_t_stamp;
 
-    int sum_in_mili = mili + 1000*sec + 1000*60*minu + 1000*60*60*hour;
+    t_stamp = stringToTimestamp(strTime);
+
+    int sum_in_mili = totalMili(t_stamp.hour, t_stamp.minu, t_stamp.sec, t_stamp.mili);
 
     sum_in_mili += delayTime;
 
-    hour = sum_in_mili/3600000;
-    minu = (sum_in_mili - hour*3600000)/60000;
-    sec = (sum_in_mili - hour*3600000 - minu*60000)/1000;
-    mili = (sum_in_mili - hour*3600000 - minu*60000 - sec*1000);
+    new_t_stamp = breakdownMili(sum_in_mili);
 
-    stringstream ss;
-    ss << setw(2) << setfill('0') << to_string(hour)
-    << ":" << setw(2) << setfill('0') << to_string(minu)
-    << ":" << setw(2) << setfill('0') << to_string(sec)
-    << "," << setw(3) << setfill('0') << to_string(mili);
-    string s = ss.str();
-    return s;
+    return timestampToString(new_t_stamp);
 }
 
 int main()
