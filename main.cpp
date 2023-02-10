6eb2b371b6b2ea90009384d95cf1de537574b263
DucDung-oreo <huynhducdung1399@gmail.com>
2023-02-10 22:15:33 +0700

    Organize the order of functions

--- main.cpp
+++ main.cpp
@@ -14,6 +14,53 @@ struct Timestamp {
     int mili;
 };
 
+int totalMili(int hour, int minu, int sec, int mili);
+Timestamp stringToTimestamp(string strTime);
+Timestamp breakdownMili(int sum_in_mili);
+string timestampToString(Timestamp t_stamp);
+string getNewTimestamp(string strTime, int delayTime);
+
+int main(int argc, char **argv)
+{
+    string myText;
+    string beginTimestamp;
+    string endTimestamp;
+    string t1;
+    string t2;
+    string newTimestamp;
+    int delayTime = atoi(argv[2]);
+    string inputPath(argv[1]);
+//    string inputPath = "/home/ducdung/Downloads/adventure-time-s01-outsync/S01E03 - Prisoners of Love.eng.srt";
+    string episodeName = inputPath.substr(inputPath.find("S01E") + 1);
+    string outputPath = "S01E" + episodeName.substr(3, 2) + "_synced.eng.srt";
+
+    ifstream MyReadFile(inputPath.c_str());
+    ofstream MyWriteFile(outputPath.c_str());
+
+//    cout << episodeName << "\n";
+
+    while(getline(MyReadFile, myText)){
+        if(myText[2]==':'){             // check if the line contain the ':' character
+
+            beginTimestamp = myText.substr(0, 12);
+            t1 = getNewTimestamp(beginTimestamp, delayTime);
+
+            endTimestamp = myText.substr(17, 12);
+            t2 = getNewTimestamp(endTimestamp, delayTime);
+
+            newTimestamp = t1 + " --> " + t2;
+            MyWriteFile << newTimestamp << "\r";
+        }
+        else{
+            MyWriteFile << myText;
+        }
+    }
+
+    MyWriteFile.close();
+    cout << "Write new subtitle to " << outputPath << ": Done!" << "\n";
+    return 0;
+}
+
 int totalMili(int hour, int minu, int sec, int mili)
 {
     return (mili + 1000*sec + 1000*60*minu + 1000*60*60*hour);
@@ -68,45 +115,4 @@ string getNewTimestamp(string strTime, int delayTime)
     new_t_stamp = breakdownMili(sum_in_mili);
 
     return timestampToString(new_t_stamp);
-}
-
-int main(int argc, char **argv)
-{
-    string myText;
-    string beginTimestamp;
-    string endTimestamp;
-    string t1;
-    string t2;
-    string newTimestamp;
-    int delayTime = atoi(argv[2]);
-    string inputPath(argv[1]);
-//    string inputPath = "/home/ducdung/Downloads/adventure-time-s01-outsync/S01E03 - Prisoners of Love.eng.srt";
-    string episodeName = inputPath.substr(inputPath.find("S01E") + 1);
-    string outputPath = "S01E" + episodeName.substr(3, 2) + "_synced.eng.srt";
-
-    ifstream MyReadFile(inputPath.c_str());
-    ofstream MyWriteFile(outputPath.c_str());
-
-//    cout << episodeName << "\n";
-
-    while(getline(MyReadFile, myText)){
-        if(myText[2]==':'){             // check if the line contain the ':' character
-
-            beginTimestamp = myText.substr(0, 12);
-            t1 = getNewTimestamp(beginTimestamp, delayTime);
-
-            endTimestamp = myText.substr(17, 12);
-            t2 = getNewTimestamp(endTimestamp, delayTime);
-
-            newTimestamp = t1 + " --> " + t2;
-            MyWriteFile << newTimestamp << "\r";
-        }
-        else{
-            MyWriteFile << myText;
-        }
-    }
-
-    MyWriteFile.close();
-    cout << "Write new subtitle to " << outputPath << ": Done!" << "\n";
-    return 0;
 }
